575b97921816decf47db9e2be8cdf16801924b38
Petryk Roman <romic.argument@gmail.com>
2021-08-11 23:43:32 +0300

    Updates

---
+++ static/js/main.a0a4143a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-01-components"]=this["webpackJsonpgoit-react-hw-01-components"]||[]).push([[0],[function(o,t){}],[[0,1]]]);
+//# sourceMappingURL=main.a0a4143a.chunk.js.map
